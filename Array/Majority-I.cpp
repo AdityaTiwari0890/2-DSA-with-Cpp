cf52b90b1d131828a2b08a195db99a5eb8f6fc11
Aditya Tiwari <aktiwari089081@gmail.com>
2026-04-20 17:08:42 +0530

    Array

--- Array/Majority-I.cpp
+++ Array/Majority-I.cpp
@@ -1,21 +1,44 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int majority(vector<int> arr){
+//     int count = 0;
+//     int num = arr[0];
+//     for(int i =0;i<arr.size();i++){
+//         if(arr[i] == num){
+//             count++;
+//         }else{
+//             count--;
+//             if(count <= 0){
+//                 num = arr[i];
+//             }
+//         }
+//     }
+//     return num;
+// }
+// int main(){
+//     vector<int> arr = {1, 1, 1, 2, 1, 2,2,2};
+//     cout<<majority(arr);
+// }
 #include<bits/stdc++.h>
 using namespace std;
-int majority(vector<int> arr){
-    int count = 0;
-    int num = arr[0];
+int majority(vector<int>arr){
+    int cnt = 0;
+    int num = INT_MAX;
     for(int i =0;i<arr.size();i++){
+        if(cnt != 0){
         if(arr[i] == num){
-            count++;
+            cnt++;
         }else{
-            count--;
-            if(count <= 0){
-                num = arr[i];
-            }
+            cnt--;
+        }
+        }else{
+        num = arr[i];
+        cnt++;
         }
     }
     return num;
 }
 int main(){
-    vector<int> arr = {1, 1, 1, 2, 1, 2,2,2};
+    vector<int> arr = {7,0,0,1,7,7,2,7,7};
     cout<<majority(arr);
 }
--- Array/count_invertion_in_array.cpp
+++ Array/count_invertion_in_array.cpp
@@ -1,6 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
+int merge(vector<int>&arr,int left,int mid, int right){
+    int l = left;
+    int r = mid+1;
+    int count = 0;
+    vector<int>result;
+    while(l<=mid && r<=right){
+        if(arr[l]<=arr[r]){
+            result.push_back(arr[l]);
+            l++;
+        }else{
+            result.push_back(arr[r]);
+            count+=(mid-l+1);
+            r++;
+        }
+    }
 
+    while(l<=mid){
+        result.push_back(arr[l]);
+        l++;
+    }
+    while(r<=right){
+        result.push_back(arr[r]);
+        r++;
+    }
+    int i = 0;
+    while(left<=right){
+        arr[left] = result[i];
+        left++;
+        i++;
+    }
+    return count;
+}
+int merge_sort(vector<int>&arr,int left, int right){
+    int count = 0;
+    if(left >= right){
+        return count;
+    }
+    int mid = left+(right-left)/2;
+    count+=merge_sort(arr,left,mid);
+    count+=merge_sort(arr,mid+1,right);
+    count+=merge(arr,left,mid,right);
+    return count;
+}
 int main(){
+    vector<int> arr = {5,3,2,1,4};
+    cout<<merge_sort(arr,0,4)<<" ";
+    for(int i = 0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
 
+    return 0;
 }
