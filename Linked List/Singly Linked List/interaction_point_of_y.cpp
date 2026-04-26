59ece71b4aae3d45840dc388e0752297114bdc40
Aditya Tiwari <aktiwari089081@gmail.com>
2026-04-27 03:59:53 +0530

    LinkedList

---
+++ Linked List/Singly Linked List/interaction_point_of_y.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+Node *common(Node *head1,Node *head2){
+    Node *h1 = head1;
+    Node *h2 = head2;
+    while(h1!=h2){
+        h1 = h1==nullptr?h2:h1->next;
+        h2 = h2==nullptr?h1:h2->next;
+    }
+    return h1;
+}
+int main(){}
