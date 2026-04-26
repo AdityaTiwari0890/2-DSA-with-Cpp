2de5ec32d2c421112d1263e675468fd41b688c09
Aditya Tiwari <aktiwari089081@gmail.com>
2026-04-26 15:00:56 +0530

    Array

---
+++ Array/no_of_subarray_xor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int xors(vector<int>&arr,int n){
+    vector<int> nums;
+    int num = 0;
+    unordered_map<int,int> mp;
+    mp[0] = 1;
+    int count  = 0;
+    for(int i =0;i<arr.size();i++){
+        num^=arr[i];
+        if(mp.find(num^n)!=mp.end()){
+            count+=mp[num^n];
+        }
+        mp[num]++;
+    }
+    return count;
+
+}
+int main(){
+
+}
